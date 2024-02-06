9b23111c4b9e5e78d152afade2b121a1f68a6d96
Prajwal Bhandari <bhandaripraj7@gmail.com>
2024-02-06 13:16:59 +0200

    Greeter usestate and text input

--- lessons/lesson 2/Greeter.tsx
+++ lessons/lesson 2/Greeter.tsx
@@ -1,11 +1,18 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput } from 'react-native'
+import React, { useState } from 'react'
 
-export default function Greeter( { name } ) {
+export default function Greeter( {} ) {
+  const [name,setName] = useState('')
 
   return (
     <View>
-      <Text>Greetings {name}</Text>
+      <Text>Greeter</Text>
+      <TextInput placeholder='Enter your name' onChangeText={text => setName(text)}
+      style={{height: 40,borderColor:"gray",borderWidth:1}} 
+      >   </TextInput>
+      {
+        name !== '' && <Text>Welcome to the world of React Native {name}</Text>
+      }
     </View>
   )
 }
